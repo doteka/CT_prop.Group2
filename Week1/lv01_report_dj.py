19bcc58caf986fbe5edca7f3563e6443611db4b2
= <doteka@naver.com>
2022-06-28 17:24:40 +0900

    init

---
+++ Week1/lv01_report_dj.py
@@ -0,0 +1,13 @@
+def solution(id_list, report, k):
+
+
+    answer = []
+    return answer
+
+
+id_list = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+k = 2
+
+answer = solution(id_list, report, k)
+# print(answer)
