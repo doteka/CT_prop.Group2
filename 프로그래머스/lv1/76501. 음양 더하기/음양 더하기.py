27976e2b482469631010127a022cacc38baa381d
Kan9_6j <34939955+doteka@users.noreply.github.com>
2022-07-29 16:39:56 +0900

    [level 1] Title: 음양 더하기, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,17 @@
+# PROGRAMMERS Lv1 음양 더하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/76501
+
+# 작성자 : 강동준
+# 최초 작성일 : 2022-07-29
+# 최종 작성일 : 2022-07-29
+
+def solution(absolutes, signs):
+    answer = 0
+    
+    for i in range(len(signs)):
+        if signs[i]:                        
+            answer += absolutes[i]      # True is Plus
+        else:
+            answer -= absolutes[i]      # False is minus
+            
+    return answer
