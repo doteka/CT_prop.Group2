5e0b5d6e61e004ef5c3638aa2b7275d05b0bffee
Kan9_6j <34939955+doteka@users.noreply.github.com>
2022-08-12 13:35:00 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -0,0 +1,31 @@
+# PROGRAMMERS LV2 기능 개발
+# https://school.programmers.co.kr/learn/courses/30/lessons/42586
+
+# 작성자 : 강동준
+# 최초 작성일 : 2022-08-12
+# 최종 작성일 : 20220-08-
+def ceil(number):
+    value = int(number)
+    if value != number:     # Check for decimal points
+        value += 1          # If there is, I will raise it
+    return value
+
+def solution(progresses, speeds):
+    answer = []
+    complet = []
+
+    for index in range(len(progresses)):
+        complet.append(ceil((100-progresses[index]) / speeds[index]))   # Calculating the number of remaining work days
+    index = 0
+    while(index < len(complet)):
+        count = 0
+        if(index+1 < len(complet) and complet[index] >= complet[index+1]):  # See if there are technologies that can be deployed together
+            for i in range(index+1, len(complet)):
+                if complet[index] >= complet[i]:                            # Distribute together if the remaining work days are smaller after
+                    count += 1
+                else:
+                    break
+        answer.append(count+1)
+        index += (count + 1)
+
+    return answer
